d8515561f32c0aab761ceb46682df0c66e96167b
yuchenlin <yuchen.lin@usc.edu>
2024-07-05 08:54:49 +0000

    scripts update with args

--- scripts/_common_api_eval.sh
+++ scripts/_common_api_eval.sh
@@ -1,15 +1,67 @@
-DATA_NAME=$1
-engine_name=$2
-model_name=$3
-model_pretty_name=$4
-n_shards=$5
-# default cot to be True 
-cot=${6:-True}
+# DATA_NAME=$1
+# engine_name=$2
+# model_name=$3
+# model_pretty_name=$4
+# n_shards=$5
+# # default cot to be True 
+# cot=${6:-True}
+# TEMP=0; TOP_P=1.0; 
 
-TEMP=0; TOP_P=1.0; MAX_TOKENS=4096; 
+# Initialize default values
+DATA_NAME=""
+model_name=""
+model_pretty_name=""
+n_shards=""
+run_name="default"
+TEMP=0
+TOP_P=1.0
+rp=1.0
+
+# Parse named arguments
+while getopts ":d:m:p:s:r:t:o:e:" opt; do
+  case $opt in
+    d) DATA_NAME="$OPTARG"
+    ;;
+    m) model_name="$OPTARG"
+    ;;
+    p) model_pretty_name="$OPTARG"
+    ;;
+    s) n_shards="$OPTARG"
+    ;;
+    r) run_name="$OPTARG"
+    ;;
+    t) TEMP="$OPTARG"
+    ;;
+    o) TOP_P="$OPTARG"
+    ;;
+    e) rp="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Check if required arguments are provided
+if [ -z "$DATA_NAME" ] || [ -z "$model_name" ] || [ -z "$model_pretty_name" ] || [ -z "$n_shards" ]; then
+  echo "Usage: $0 -d DATA_NAME -m model_name -p model_pretty_name -s n_shards [-r run_name] [-t TEMP] [-o TOP_P] [-e rp]"
+  exit 1
+fi
+
+
+
+
+
+MAX_TOKENS=4096; 
 batch_size=4; 
 CACHE_DIR=${HF_HOME:-"default"}
-output_dir="result_dirs/${DATA_NAME}/cot=${cot}/" 
+# output_dir="result_dirs/${DATA_NAME}/cot=${cot}/" 
+
+if [ "$run_name" = "default" ]; then
+    output_dir="result_dirs/${DATA_NAME}/" 
+else
+    output_dir="result_dirs/${DATA_NAME}/${run_name}/" 
+fi
+
 
 # If the n_shards is 1, then we can directly run the model
 # else, use  Data-parallellism
@@ -18,10 +70,9 @@ if [ $n_shards -eq 1 ]; then
     CUDA_VISIBLE_DEVICES=$gpu \
     python src/unified_infer.py \
         --data_name $DATA_NAME \
-        --cot $cot \
         --engine $engine_name \
         --model_name $model_name \
-        --top_p $TOP_P --temperature $TEMP \
+        --top_p $TOP_P --temperature $TEMP --repetition_penalty $rp \
         --batch_size $batch_size --max_tokens $MAX_TOKENS \
         --output_folder $output_dir/  
 
@@ -34,11 +85,10 @@ elif [ $n_shards -gt 1 ]; then
             --num_shards $n_shards \
             --shard_id $shard_id \
             --data_name $DATA_NAME \
-            --cot $cot \
             --engine $engine_name \
             --model_name $model_name \
             --model_pretty_name $model_pretty_name \
-            --top_p $TOP_P --temperature $TEMP \
+            --top_p $TOP_P --temperature $TEMP --repetition_penalty $rp \
             --batch_size $batch_size --max_tokens $MAX_TOKENS \
             --output_folder $shards_dir/ \
               &
--- scripts/_common_eval.sh
+++ scripts/_common_eval.sh
@@ -1,16 +1,60 @@
-DATA_NAME=$1
-model_name=$2
-model_pretty_name=$3
-n_shards=$4
-run_name=${5:-"default"}
-TEMP=${6:-0}
-TOP_P=${7:-1.0}
-rp=${8:-1.0} 
+# DATA_NAME=$1
+# model_name=$2
+# model_pretty_name=$3
+# n_shards=$4
+# run_name=${5:-"default"}
+# TEMP=${6:-0}
+# TOP_P=${7:-1.0}
+# rp=${8:-1.0} 
+
+
+#!/bin/bash
+
+# Initialize default values
+DATA_NAME=""
+model_name=""
+model_pretty_name=""
+n_shards=""
+run_name="default"
+TEMP=0
+TOP_P=1.0
+rp=1.0
+
+# Parse named arguments
+while getopts ":d:m:p:s:r:t:o:e:" opt; do
+  case $opt in
+    d) DATA_NAME="$OPTARG"
+    ;;
+    m) model_name="$OPTARG"
+    ;;
+    p) model_pretty_name="$OPTARG"
+    ;;
+    s) n_shards="$OPTARG"
+    ;;
+    r) run_name="$OPTARG"
+    ;;
+    t) TEMP="$OPTARG"
+    ;;
+    o) TOP_P="$OPTARG"
+    ;;
+    e) rp="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# Check if required arguments are provided
+if [ -z "$DATA_NAME" ] || [ -z "$model_name" ] || [ -z "$model_pretty_name" ] || [ -z "$n_shards" ]; then
+  echo "Usage: $0 -d DATA_NAME -m model_name -p model_pretty_name -s n_shards [-r run_name] [-t TEMP] [-o TOP_P] [-e rp]"
+  exit 1
+fi
+
 
 MAX_TOKENS=4096; 
 batch_size=4; 
 CACHE_DIR=${HF_HOME:-"default"}
-if run_name == "default"; then
+if [ "$run_name" = "default" ]; then
     output_dir="result_dirs/${DATA_NAME}/" 
 else
     output_dir="result_dirs/${DATA_NAME}/${run_name}/" 
