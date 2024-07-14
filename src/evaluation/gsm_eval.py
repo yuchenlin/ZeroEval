7731896862293d0854004197dcae3244fea81334
yuchenlin <yuchen.lin@usc.edu>
2024-07-14 04:18:04 +0000

    update the logic of value extraction

--- src/evaluation/gsm_eval.py
+++ src/evaluation/gsm_eval.py
@@ -4,7 +4,7 @@ import os
 from tabulate import tabulate 
 import re
  
-from eval_utils import load_model_results, extract_values_from_json
+from eval_utils import load_model_results, extract_values_from_json, extract_last_complete_json
  
 
 def santize_math_answers(answer):
@@ -34,8 +34,11 @@ def eval_model(model, filepath):
     reason_lens = []
     for item in data:  
         # Read and Parse the prediction from model output
-        prediction_str = item["output"][0]     
-        prediction_json = extract_values_from_json(prediction_str)
+        prediction_str = item["output"][0]
+        prediction_json = extract_last_complete_json(prediction_str)
+        if prediction_json is None or "answer" not in prediction_json:
+            prediction_json = extract_values_from_json(prediction_str)
+            # print("-")
         if prediction_json is None or "answer" not in prediction_json: 
             no_asnwer += 1 
             if False and  "claude-3-5-sonnet-20240620" in model:
