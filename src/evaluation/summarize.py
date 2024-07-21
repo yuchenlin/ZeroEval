9c8b0b28923ce4e0365fabd1294d805c53b31ec7
yuchenlin <yuchen.lin@usc.edu>
2024-07-21 20:38:41 +0000

    no index

--- src/evaluation/summarize.py
+++ src/evaluation/summarize.py
@@ -53,8 +53,13 @@ merged_data['Average'] = merged_data.drop(columns=['Model']).mean(axis=1)
 merged_data = merged_data.sort_values(by='Average', ascending=False)
 
 # Generate a Markdown table
-markdown_table = merged_data.to_markdown(index=True, floatfmt=".2f")
+markdown_table = merged_data.to_markdown(index=False, floatfmt=".2f")
 
-print(markdown_table)
+# rename the <br> in column names with "\n"
+merged_data.columns = merged_data.columns.str.replace('<br/>', '\n')
+markdown_table_lite = merged_data.to_markdown(index=False, floatfmt=".2f", tablefmt="grid")
+print(markdown_table_lite)
+
+# print(markdown_table)
 with open('result_dirs/summary.md', 'w') as f:
     f.write(markdown_table)
