363238ba58a4aef2acfb5385cd51369451e8ad0a
yuchenlin <yuchen.lin@usc.edu>
2024-08-06 12:37:33 -0700

    add new results

--- scripts/local/athena.sh
+++ scripts/local/athena.sh
@@ -1,3 +1,4 @@
 bash zero_eval_local.sh -f vllm -d gsm -m Nexusflow/Athene-70B -p Athene-70B -s 1
 bash zero_eval_local.sh -f vllm -d mmlu-redux -m Nexusflow/Athene-70B -p Athene-70B -s 1
 bash zero_eval_local.sh -f vllm -d zebra-grid -m Nexusflow/Athene-70B -p Athene-70B -s 1
+bash zero_eval_local.sh -f vllm -d crux -m Nexusflow/Athene-70B -p Athene-70B -s 1
---
+++ scripts/local/hyperbolic.sh
@@ -0,0 +1,8 @@
+bash zero_eval_api.sh -f openai -d gsm -m meta-llama/Meta-Llama-3.1-405B-Instruct@hyperbolic -p Llama-3.1-405B-Instruct-BF16 -s 1
+wait
+bash zero_eval_api.sh -f openai -d crux -m meta-llama/Meta-Llama-3.1-405B-Instruct@hyperbolic -p Llama-3.1-405B-Instruct-BF16 -s 1
+wait
+bash zero_eval_api.sh -f openai -d zebra-grid -m meta-llama/Meta-Llama-3.1-405B-Instruct@hyperbolic -p Llama-3.1-405B-Instruct-BF16 -s 1
+wait
+bash zero_eval_api.sh -f openai -d mmlu-redux -m meta-llama/Meta-Llama-3.1-405B-Instruct@hyperbolic -p Llama-3.1-405B-Instruct-BF16 -s 1
+wait
