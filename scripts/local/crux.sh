cc38aa065b21c4ef53fb534bdb6ad9912fc88317
yuchenlin <yuchen.lin@usc.edu>
2024-09-09 16:27:45 +0000

    summarize

--- scripts/local/crux.sh
+++ scripts/local/crux.sh
@@ -19,6 +19,7 @@
 # bash zero_eval_api.sh -d crux -f google -m google/gemini-1.5-pro -p gemini-1.5-pro -s 8
 # bash zero_eval_api.sh -d crux -f google -m google/gemini-1.5-flash -p gemini-1.5-flash -s 8
 # bash zero_eval_api.sh -f google -d crux -m google/gemini-1.5-pro-exp-0801 -p gemini-1.5-pro-exp-0801 -s 1
+bash zero_eval_api.sh -f google -d crux -m google/gemini-1.5-pro-exp-0827 -p gemini-1.5-pro-exp-0827 -s 1
 
 
 
